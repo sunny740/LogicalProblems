88e6b3c984557ed21e3f6a79f498952d4f3d2f9c
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-16 16:30:41 +0530

    [Sunny] Add : Welcome To The logical Programs

---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Snake_and_Ladder
+{
+    public class PlayerPostionZero
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The logical Programs");
+            Console.ReadLine();
+        }
+    }
+}
