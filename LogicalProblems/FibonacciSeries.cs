daa3b2534240fb4def4cb2355489815fe49fb080
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-16 16:39:27 +0530

    [Sunny] Add : Fibonacci Series implementation

---
+++ LogicalProblems/FibonacciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            int num1 = 0, num2 = 1, sum = 0;
+            Console.WriteLine(num1 + "" + num2);
+            for (int i = 2; i < 10; i++)
+            {
+                sum = num1 + num2;
+                num1 = num2;
+                num2 = sum;
+                Console.WriteLine(" " + sum);
+            }
+        }
+    }
+}
--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -1,13 +1,62 @@
 ﻿using System;
 
-namespace Snake_and_Ladder
+namespace LogicalProblems
 {
-    public class PlayerPostionZero
+    public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To The logical Programs");
-            Console.ReadLine();
+            Console.WriteLine("Welcome to Logical Problems");
+            bool end = true;
+            Console.WriteLine("1. Fibonacci Series\n2. Perfect Numbers\n3. Prime Number\n4. Reverse Numbers\n5. Day Of Week\n6. Tempreture\n7. Monthly Payment\n8. Monthly Payment2\n9. Vending Machine\n");
+            while (end == true)
+            {
+                Console.WriteLine("Take an option to execute");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries flip = new FibonacciSeries();
+                        flip.Fibonacci();
+                        break;
+                    //case 2:
+                    //    PerfectNumber per = new PerfectNumber();
+                    //    per.Number();
+                    //    break;
+                    //case 3:
+                    //    PrimeNumbers Number = new PrimeNumbers();
+                    //    Number.Prime();
+                    //    break;
+                    //case 4:
+                    //    ReverseNumber Rvs = new ReverseNumber();
+                    //    Rvs.Reverse();
+                    //    break;
+                    //case 5:
+                    //    DayOfWeek Day = new DayOfWeek();
+                    //    Day.Week();
+                    //    break;
+                    //case 6:
+                    //    Tempreture TEMP = new Tempreture();
+                    //    TEMP.Temp();
+                    //    break;
+                    //case 7:
+                    //    MonthlyPayment Pay = new MonthlyPayment();
+                    //    Pay.Payment();
+                    //    break;
+                    //case 8:
+                    //    MonthlyPayment PAy = new MonthlyPayment();
+                    //    PAy.Payment();
+                    //    break;
+                    //case 9:
+                    //    VendingMachine Ven = new VendingMachine();
+                    //    Ven.Machine();
+                    //    break;
+                    default:
+                        Console.WriteLine("Enter Proper Option To Execute");
+                        Console.WriteLine("Choose Number Between 1-9");
+                        break;
+                }
+            }
         }
     }
 }
