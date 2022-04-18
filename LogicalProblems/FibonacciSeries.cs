05438675fa598b98167f8acdfd1d0027d32a54e7
sunny740 <sunnysejwarvitm005@gmail.com>
2022-04-19 01:48:32 +0530

    [Sunny] Refactor : Fibonacci Series updated

--- LogicalProblems/FibonacciSeries.cs
+++ LogicalProblems/FibonacciSeries.cs
@@ -10,14 +10,17 @@ namespace LogicalProblems
     {
         public void Fibonacci()
         {
-            int num1 = 0, num2 = 1, sum = 0;
-            Console.WriteLine(num1 + "" + num2);
-            for (int i = 2; i < 10; i++)
+            int num1 = 0, num2 = 1, sum;
+            Console.WriteLine("Enter The Value Of Series");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibonacci Series: ");
+            Console.WriteLine(+num1);
+            for (int i = 1; i < num; i++)
             {
                 sum = num1 + num2;
+                Console.WriteLine(" " + sum);
                 num1 = num2;
                 num2 = sum;
-                Console.WriteLine(" " + sum);
             }
         }
     }
